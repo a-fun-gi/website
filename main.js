336d1ae412943d503e7c578abc753ac86b654fcb
a-fun-gi <175504279+a-fun-gi@users.noreply.github.com>
2026-07-10 17:34:57 -0700

    edit before first devlog

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
+var thing = "";
 async function update_time() {
     while (1==1) {
-        const now = new Date().toLocaleTimeString()
-        document.getElementById("timestamp").textContent = now;
+        const now = new Date().toLocaleTimeString();
+        const hour = new Date().getHours();
+        if (0<hour<6 || 20<hour<23) {
+            thing = "not sleeping";
+        }
+        else if (not(0<hour<6 || 20<hour<23)) {
+            thing = "sleeping"
+        }
+        document.getElementById("timestamp").textContent = "at " + now + " I am most likely " + thing;
         await sleep(1000);
     }
 }
